05cbf01ec0c8103c67129cb53369355d1645d3ef
Areeg <Areegaltaiyah@gmail.com>
2025-10-16 10:49:37 +0300

    Initial Commit

---
+++ Learning/LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningApp.swift
+//  Learning
+//
+//  Created by Areeg Altaiyah on 24/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
